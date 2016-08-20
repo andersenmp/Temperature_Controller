d93819f2deffce3c57254c9fdee442e3348d1661
Andersen Pecorone <andersenmp@gmail.com>
2016-08-21 00:50:36 +0200

    Change static files to static folder, new classes for hardware

--- API_hardware/jsonLog.py
+++ API_hardware/jsonLog.py
@@ -2,7 +2,7 @@ import json, datetime
 
 
 def log(data):
-    f = 'log/app_log_' + datetime.datetime.now().strftime("%Y-%m-%d") + '.log'
+    f = 'static/download/app_log_' + datetime.datetime.now().strftime("%Y-%m-%d") + '.log'
     with open(f, 'a') as outfile:
         json.dump(data, outfile)
         outfile.write("\n")
