ac30201c2c6730266f7de02e5901d2be3064ee3e
Andersen Pecorone <andersen.pecorone@atenta.eu>
2017-07-29 16:01:09 +0200

    Log

--- API_hardware/jsonLog.py
+++ API_hardware/jsonLog.py
@@ -4,6 +4,7 @@ import os
 
 
 def log(data):
+    '''
     filedir = os.path.dirname(os.path.realpath('__file__'))
     filename =  'files_download/day_' + datetime.datetime.now().strftime("%Y-%m-%d") + '.txt'
     f = os.path.join(filedir, filename)
@@ -12,5 +13,5 @@ def log(data):
         json.dump(data, outfile)
         outfile.write("\n")
     outfile.close()
-
+    '''
 
