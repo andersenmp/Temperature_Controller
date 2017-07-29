b807729d724fce16d2875fc0f34895431266a326
Andersen Pecorone <andersen.pecorone@atenta.eu>
2017-07-29 15:47:07 +0200

    Log

--- API_hardware/jsonLog.py
+++ API_hardware/jsonLog.py
@@ -4,9 +4,10 @@ import os
 
 
 def log(data):
-    fileDir = os.path.dirname(os.path.realpath('__file__'))
+    filedir = os.path.dirname(os.path.realpath('__file__'))
     filename =  'files_download/day_' + datetime.datetime.now().strftime("%Y-%m-%d") + '.txt'
-    f = os.path.join(fileDir, filename)
+    f = os.path.join(filedir, filename)
+    print(f)
     with open(f, 'a') as outfile:
         json.dump(data, outfile)
         outfile.write("\n")
